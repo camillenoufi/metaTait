0119e49e981ca11946d6f5db182f80cb2b97b4e3
masondarv <robert.darveaux@colorado.edu>
2017-02-19 15:23:04 -0700

    Updated High Speed SPI and added MCBSP DMA configuration and functions

---
+++ Post_MCU/CPU1_RAM/LEDControl.d
@@ -0,0 +1,5 @@
+# FIXED
+
+Example_2837xSSpi_FFDLB.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_examples_Cpu1/spi_loopback/cpu01/Example_2837xSSpi_FFDLB.c
+
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_examples_Cpu1/spi_loopback/cpu01/Example_2837xSSpi_FFDLB.c: 
---
+++ Post_MCU/CPU1_RAM/Post_MCU.d
@@ -0,0 +1,111 @@
+# FIXED
+
+Post_MCU.obj: ../Post_MCU.c
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F28x_Project.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Cla_typedefs.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_device.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/assert.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/linkage.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdarg.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h
+Post_MCU.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_adc.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_analogsubsys.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cla.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cmpss.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cputimer.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dac.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dcsm.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dma.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_ecap.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_emif.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_epwm.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_epwm_xbar.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_eqep.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_flash.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_gpio.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_i2c.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_input_xbar.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_mcbsp.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_memconfig.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_nmiintrupt.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_output_xbar.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_piectrl.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_pievect.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sci.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sdfm.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_spi.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sysctrl.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_upp.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_xbar.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_xint.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Examples.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_GlobalPrototypes.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_cputimervars.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Cla_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_EPwm_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Adc_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Emif_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Gpio_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_I2c_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Pie_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Dma_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_SysCtrl_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Upp_defines.h
+Post_MCU.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_defaultisr.h
+
+../Post_MCU.c: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F28x_Project.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Cla_typedefs.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_device.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/assert.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_adc.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_analogsubsys.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cla.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cmpss.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_cputimer.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dac.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dcsm.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_dma.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_ecap.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_emif.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_epwm.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_epwm_xbar.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_eqep.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_flash.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_gpio.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_i2c.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_input_xbar.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_mcbsp.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_memconfig.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_nmiintrupt.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_output_xbar.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_piectrl.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_pievect.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sci.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sdfm.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_spi.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_sysctrl.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_upp.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_xbar.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/include/F2837xS_xint.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Examples.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_GlobalPrototypes.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_cputimervars.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Cla_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_EPwm_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Adc_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Emif_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Gpio_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_I2c_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Pie_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Dma_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_SysCtrl_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_Upp_defines.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F2837xS_defaultisr.h: 
