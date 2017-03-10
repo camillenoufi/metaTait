6465bfd118d23c539d9f027dcd76f8de778a0c92
camillenoufi <camille.noufi@colorado.edu>
2017-03-09 17:18:38 -0700

    uint16 array modifications for chunk transfer

--- Post_MCU/CPU1_RAM/F2837xS_GlobalVariableDefs.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-F2837xS_GlobalVariableDefs.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/source/F2837xS_GlobalVariableDefs.c
-
-C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/source/F2837xS_GlobalVariableDefs.c: 
--- Post_MCU/CPU1_RAM/F2837xS_Gpio.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-F2837xS_Gpio.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/source/F2837xS_Gpio.c
-
-C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/source/F2837xS_Gpio.c: 
